8cac84cfdea256718613e222faaecab01150de51
jesse <goosetherumfoodle@users.noreply.github.com>
2018-02-24 18:16:49 -0500

    adds initial redux plumbing

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Provider} from 'react-redux'
+import registerServiceWorker from './registerServiceWorker'
+import {createStore, compose, applyMiddleware} from 'redux'
 import './index.css';
+import thunk from 'redux-thunk'
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import reducer from './store/reducer'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(
+  reducer,
+  composeEnhancers(
+    applyMiddleware(thunk)
+  )
+)
+
+ReactDOM.render(<Provider store={store}>
+                <App />
+                </Provider>,
+                document.getElementById('root'))
 registerServiceWorker();
